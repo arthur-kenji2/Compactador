908fd63499d543d7d498dc65984e0cff6123827f
kenjinBomDeBola <43621085+kenjinBomDeBola@users.noreply.github.com>
2019-10-23 11:36:36 -0300

    a

--- CompactadorC/main.c
+++ CompactadorC/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define ' ' sp
 
 int main()
 {
+
     typedef struct
     {
         char letra;
@@ -10,6 +12,7 @@ int main()
     } LetraFreq;
 
     LetraFreq noLetra[255];
+    int qtd = 0;
 
 
     for(int i=0; i<255; i++)
@@ -29,18 +32,48 @@ int main()
             {
                 if(noLetra[i].letra == 0)
                 {
+                    if(noLetra[i].letra == " ")
                     noLetra[i].letra = c;
                     noLetra[i].freq  = 1;
+                    qtd++;
                     break;
                 }
 
                 if(noLetra[i].letra == c)
+                {
                     noLetra[i].freq += 1;
-                printf("%c", noLetra[i].letra);
+                    break;
+                }
 
             }
         fclose(file);
     }
+    printf("Desordenado:\n");
+    for(int i = 0; i < qtd; i++)
+        for(int i = 0; i < qtd; i++)
+        {
+            printf("%c - ", noLetra[i].letra);
+            printf("%d\n", noLetra[i].freq);
+        }
+
+    for (int i = 0; i < qtd; ++i)
+        for (int j = i + 1; j < qtd; ++j)
+            if (noLetra[i].freq > noLetra[j].freq)
+            {
+                int a =  noLetra[i].freq;
+                char c = noLetra[i].letra;
+                noLetra[i].freq = noLetra[j].freq;
+                noLetra[i].letra = noLetra[j].letra;
+                noLetra[j].freq = a;
+                noLetra[j].letra = c;
+            }
+
+    printf("Ordenado:\n");
+        for(int i = 0; i < qtd; i++)
+        {
+            printf("%c - ", noLetra[i].letra);
+            printf("%d\n", noLetra[i].freq);
+        }
 
 
 
